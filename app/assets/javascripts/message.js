b5905ca51807e9ce74fb1d765c653ef95b21879f
sample <rari6217@yahoo.co.jp>
2019-06-25 20:05:01 +0900

    up

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 function buildHTML(message){
-    var image = message.image? ${message.image} : ""
+    //var image = message.image? ${message.image} : ""
   
     var html =
      `<div class="message" data-message-id=${message.id}>
@@ -43,3 +43,68 @@ $.ajax({
   });
   return false;
 });
+
+
+
+(function() {
+
+  function buildHTML(message){
+    var html = `<div class="message">
+                           <div class="upper-message">
+                               <div class="upper-message__user-name">
+                                   ${ message.user_name }
+                               </div>
+                               <div class="upper-message__date">
+                                   ${ message.time }
+                               </div>
+                           </div>
+                           <div class="lower-message">
+                               <p class="lower-message__content">
+                                   ${ message.content }
+                               </p>
+                           </div>
+                       </div>`;
+        return html;
+    
+    }
+    function scroll() {
+
+      $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
+  
+  }
+
+  $('#new_message').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+  })
+
+})
+$("#new_message").on('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+  $.ajax({
+  url: url,
+  type: "POST",
+  data: formData,
+  dataType: 'json',
+  processData: false,
+  contentType: false
+  })
+  .done(function(data){
+
+    var html = buildHTML(data);
+
+    $('.messages').append(html);
+    $('.form__message').val('');
+    $('.form__submit').prop('disabled', false);
+    scroll()
+
+})
+.fail(function(){
+  alert('error')
+  $('.form__submit').prop('disabled', false);
+})
+
+})
