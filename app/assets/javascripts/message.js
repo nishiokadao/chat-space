1b68f0721ff5002ab1ffa9f99b5b13b76cffe6aa
sample <rari6217@yahoo.co.jp>
2019-06-30 13:13:59 +0900

    updata

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,113 +1,62 @@
-function buildHTML(message){
-    var image = message.image? ${message.image} : ""
-  
-    var html =
-     `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-          ${image}
-        </div>
-      </div>`
-    return html;
-}
-$('.js-form').on('submit', function(){
-e.preventDefault();
-var formData = new FormData(this);
-var url = $(this).attr('action')
-$.ajax({
-  url: url,
-  type: "POST",
-  data: formData,
-  dataType: 'json',
-  processData: false,
-  contentType: false
-})
- .done(function(data){
-   var html = buildHTML(data);
-   $('.messages').append(html);
-   $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-   $('form')[0].reset();
- })
-  .fail(function(){
-    alert('error');
-  });
-  return false;
-});
-
-
-
-(function() {
+$(function() {
 
   function buildHTML(message){
-    var image = message.image.url? $(message.image.url) :""
-    var html = `<div class="message">
-                           <div class="upper-message">
-                               <div class="upper-message__user-name">
-                                   ${ message.user_name }
-                               </div>
-                               <div class="upper-message__date">
-                                   ${ message.time }
-                               </div>
-                           </div>
-                           <div class="lower-message">
-                               <p class="lower-message__content">
-                                   ${ message.content }
-                               </p>
-                               <img src="${image}">
-                           </div>
-                       </div>`;
-        return html;
+      var image = message.image? `${message.image}` : ""
     
-    }
-    function scroll() {
-
-      $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
-  
+      var html =
+      `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
+      return html;
   }
 
-  $('#new_message').on('submit', function(e) {
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action');
+
+
+  function scroll() {
+    $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
+  }
+
+
+  $(".new_message").on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+    })
+    .done(function(data){
+      console.log("done")
+      var html = buildHTML(data);
+
+      $('.messages').append(html);
+      // $('.form__message').input[type=“reset”];
+      $('.form__submit').prop('disabled', false);
+      scroll()
+
   })
-
-})
-$("#new_message").on('submit', function(e) {
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action');
-  $.ajax({
-  url: url,
-  type: "POST",
-  data: formData,
-  dataType: 'json',
-  processData: false,
-  contentType: false
-  })
-  .done(function(data){
-
-    var html = buildHTML(data);
-
-    $('.messages').append(html);
-    $('.form__message').input[type=“reset”];
+  .fail(function(){
+    alert('error')
     $('.form__submit').prop('disabled', false);
-    scroll()
+  })
+      return false;
+  })
 
-})
-.fail(function(){
-  alert('error')
-  $('.form__submit').prop('disabled', false);
-})
-    return false;
-})
-
+});
