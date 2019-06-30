803994165ae85a6338944cf898ea32df2bb0ac09
sample <rari6217@yahoo.co.jp>
2019-06-30 17:07:12 +0900

    up

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -57,4 +57,29 @@ $(function() {
       return false;
   })
 
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
+      // var group_id = $(".group").data("group-id");
+
+      $.ajax({ 
+        url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
+      })
+      .done(function (messages) { 
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
